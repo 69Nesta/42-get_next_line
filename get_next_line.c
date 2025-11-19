0070226197b05a5418be20aa990ac5734d9b3631
Romeo Petit <rpetit@z4r5p5.42lyon.fr>
2025-11-19 13:36:12 +0100

    Chore: make get_next_line work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rpetit <rpetit@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 08:37:44 by rpetit            #+#    #+#             */
-/*   Updated: 2025/11/18 16:39:08 by rpetit           ###   ########.fr       */
+/*   Updated: 2025/11/19 13:27:22 by rpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #include <stdio.h>
 
 static char		*ft_realloc(char *src, size_t size, size_t add_size);
-static size_t	fill_line(char *line, char *buffer);
-static char		*ft_free_on_fail(char *line);
+static size_t	ft_fill_line(char *line, char *buffer, size_t size);
+// static char		*ft_free_on_fail(char *line);
 /*
 	
 	1. [v] - check if there is an old read if not read
@@ -34,7 +34,7 @@ typedef struct s_gnl
 {
 	ssize_t buffer_index;
 	char	buffer[BUFFER_SIZE];
-	ssize_t	read_value;
+	ssize_t	bytes_read;
 }	t_gnl;
 
 char	*get_next_line(int fd)
@@ -42,127 +42,66 @@ char	*get_next_line(int fd)
 	static t_gnl	gnl = {
 		.buffer = "",
 		.buffer_index = 0,
-		.read_value = 0
+		.bytes_read = 0
 	};
 	char	*line;
+	size_t	line_size;
 	size_t	len_to_nl;
-	size_t	line_len;
 
-	if (read(fd, 0, 0) < 0)
-		return (NULL);
-	if (!gnl.read_value)
-		gnl.read_value = read(fd, gnl.buffer, BUFFER_SIZE);
-	if (gnl.read_value <= 0)
-		return (NULL);
-	len_to_nl = ft_strlen_nl(gnl.buffer + gnl.buffer_index, BUFFER_SIZE - gnl.buffer_index);
-	line = ft_realloc(0, 0, len_to_nl);
-	line_len = fill_line(line, gnl.buffer + gnl.buffer_index);
-	if (len_to_nl != (size_t)gnl.read_value)
-		gnl.buffer_index += len_to_nl + 1;
-	// __builtin_printf("%zd - %zu - %zd - - \n", gnl.buffer_index, len_to_nl, gnl.read_value);
-	while (gnl.buffer_index == gnl.read_value + 1)
+	line = NULL;
+	line_size = 0;
+	while (1)
 	{
-		gnl.buffer_index = 0;
-		gnl.read_value = read(fd, gnl.buffer, BUFFER_SIZE);
-		if (gnl.read_value <= 0)
-			return (ft_free_on_fail(line));
-		len_to_nl = ft_strlen_nl(gnl.buffer, BUFFER_SIZE);
-		line = ft_realloc(line, line_len, len_to_nl);
-		line_len += fill_line(line, gnl.buffer);
-		// __builtin_printf("%zu - - - \n", len_to_nl);
+		if (read(fd, 0, 0) < 0)
+			return (NULL);
+		if (gnl.buffer_index >= gnl.bytes_read)
+		{
+			gnl.bytes_read = read(fd, gnl.buffer, BUFFER_SIZE);
+			if (gnl.bytes_read <= 0)
+				return (NULL);
+			gnl.buffer_index = 0;
+		}
+		len_to_nl = ft_strlen_nl(gnl.buffer + gnl.buffer_index, gnl.bytes_read - gnl.buffer_index);
+		/*
+		__builtin_printf("\n%d | %zu - %zd | %zu\n",
+			gnl.buffer[gnl.buffer_index + len_to_nl] == '\n',
+			len_to_nl,
+			BUFFER_SIZE - gnl.buffer_index,
+			line_size
+		);*/
 		
+		line = ft_realloc(line, line_size, len_to_nl + (gnl.buffer[gnl.buffer_index + len_to_nl] == '\n'));
+		if (!line)
+			return (line);
+		line_size += ft_fill_line(line + line_size, gnl.buffer + gnl.buffer_index, len_to_nl);
+		if (len_to_nl != (size_t) BUFFER_SIZE - gnl.buffer_index)
+		{
+			gnl.buffer_index += len_to_nl + 1;
+			return (line);
+		}
+		else
+			gnl.buffer_index += len_to_nl;
 	}
-	
 	return (line);
 }
 
-static char	*ft_free_on_fail(char *line)
-{
-	free(line);
-	return (NULL);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// char	*get_next_line(int fd)
-// {
-// 	static t_gnl	gnl;
-// 	char			*line;
-// 	size_t			bytes_in_line;
-// 	size_t			fill_size;
-
-// 	__builtin_printf("\n");
-// 	bytes_in_line = 0;
-// 	__builtin_printf("[start] %zu - %zu | %zu\n", gnl.buffer_index, gnl.read_value, bytes_in_line);
-// 	if (gnl.read_value == gnl.buffer_index)
-// 		gnl.read_value = read(fd, gnl.buffer, BUFFER_SIZE);
-// 	if (gnl.read_value == 0)
-// 		return (NULL);
-// 	line = ft_realloc(0, 0, gnl.read_value - gnl.buffer_index);
-// 	fill_size = fill_line(line + bytes_in_line, gnl.buffer + gnl.buffer_index);
-// 	gnl.buffer_index += fill_size;
-// 	bytes_in_line += fill_size;
-
-// 	__builtin_printf("[ end ] %zu - %zu | %zu\n", gnl.buffer_index , gnl.read_value, bytes_in_line);
-// 	while (gnl.buffer_index == gnl.read_value)
-// 	{
-// 		gnl.buffer_index = 0;
-// 		gnl.read_value = read(fd, gnl.buffer, BUFFER_SIZE);
-// 		line = ft_realloc(line, bytes_in_line, gnl.read_value);
-// 		fill_size = fill_line(line + bytes_in_line, gnl.buffer + gnl.buffer_index);
-// 		gnl.buffer_index += fill_size;
-// 		bytes_in_line += fill_size;
-// 		__builtin_printf("%zu - %zu | %zu\n", gnl.buffer_index, gnl.read_value, bytes_in_line);
-// 	}
-// 	return (line);
-// }
-
-static size_t	fill_line(char *line, char *buffer)
+static size_t	ft_fill_line(char *line, char *buffer, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	if (!line)
+	if (!line && size > 0)
 		return 0;
-	while (buffer[i])
+	while (i < size)
 	{
 		line[i] = buffer[i];
-		if (buffer[i] == '\n')
-			break ;
 		i++;
 	}
-	line[i + 1] = '\0';
-	return (i + (buffer[i] == '\n'));
+	// __builtin_printf("Fill li: %zu - %zu\n\n", i, size);
+	if (buffer[size] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (i + (buffer[size] == '\n'));
 }
 
 static char	*ft_realloc(char *src, size_t size, size_t add_size)
@@ -171,9 +110,13 @@ static char	*ft_realloc(char *src, size_t size, size_t add_size)
 	char	*new_src;
 
 	i = 0;
+	// __builtin_printf("Realloc: %zu - %zu\n", size, add_size);
 	new_src = malloc(sizeof(char) * (size + add_size + 1));
 	if (!new_src)
+	{
+		free(src);
 		return NULL;
+	}
 	while (i < size)
 	{
 		new_src[i] = src[i];
